40bf05b12cb2d14dc5fdbac666ac29806de6b3e5
HugoCerrillo <hugolcano85@gmail.com>
2026-06-22 10:18:56 -0600

    version estable

--- src/hooks/back_events_management.js
+++ src/hooks/back_events_management.js
@@ -34,8 +34,6 @@ export function useEventsManagement() {
       if (dataEventos.status !== 'success') {
         throw new Error(dataEventos.message || 'Error cargando eventos');
       }
-      //mostrar en consola los mantenimientos obteneidos (verificar respuesta del backend      
-      console.log(dataMant);
 
       //obtener los datos de los eventos, diagnosticos, mantenimientos y equipos
       const eventosList = dataEventos.eventos || [];
